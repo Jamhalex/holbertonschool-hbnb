0cd88fe170ae4cacd35c6fbdaf7d90d947ff60d7
johnson <jamhalex@gmail.com>
2026-07-11 05:28:08 -0400

    Initialize HBnB project structure and Flask application

--- part2/app/__init__.py
+++ part2/app/__init__.py
@@ -1,20 +1,16 @@
-#!/usr/bin/python3
 from flask import Flask
-
-from app.api import init_api
-from app.persistence.repository import InMemoryRepository
-from app.services.facade import HBnBFacade
+from flask_restx import Api
 
 
 def create_app():
     app = Flask(__name__)
 
-    # In-memory repo + facade singletons for Part 2
-    repo = InMemoryRepository()
-    facade = HBnBFacade(repo)
+    api = Api(
+        app,
+        version="1.0",
+        title="HBnB API",
+        description="HBnB Application API",
+        doc="/api/v1/"
+    )
 
-    # Attach to app for access in namespaces
-    app.config["FACADE"] = facade
-
-    init_api(app)
     return app
--- part2/app/models/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-from app.models.base import BaseModel
-from app.models.user import User
-from app.models.amenity import Amenity
-from app.models.place import Place
-from app.models.review import Review
-
-__all__ = ["BaseModel", "User", "Amenity", "Place", "Review"]
